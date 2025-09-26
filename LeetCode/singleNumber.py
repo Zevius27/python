684ddb287611c5aa1aba6ebc48bc39b3e7bec372
Zevius27 <zeviusaplha@gmail.com>
2025-09-26 19:44:30 +0530

    some more leet code

---
+++ LeetCode/singleNumber.py
@@ -0,0 +1,62 @@
+
+
+# Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+
+# You must implement a solution with a linear runtime complexity and use only constant extra space.
+
+
+# Example 1:
+
+# Input: nums = [2, 2, 1]
+
+# Output: 1
+
+# Example 2:
+
+# Input: nums = [4, 1, 2, 1, 2]
+
+# Output: 4
+
+# Example 3:
+
+# Input: nums = [1]
+
+# Output: 1
+
+
+# Constraints:
+
+# 1 <= nums.length <= 3 * 104
+# -3 * 104 <= nums[i] <= 3 * 104
+# Each element in the array appears twice except for one element which appears only once.
+
+
+class Solution:
+    def singleNumber(self, nums: list[int]) -> int:
+        freq = {}
+        for i in nums:
+            if i in freq:
+                freq[i] += 1
+            else:
+                freq[i] = 1
+        # Return the number that appears once
+        for key, val in freq.items():
+            if val == 1:
+                return key
+
+s = Solution()
+        
+s.singleNumber([2,2,1])
+        
+
+
+
+
+
+
+
+
+
+
+
+
